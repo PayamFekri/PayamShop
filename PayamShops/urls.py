07a4ee2cf475c3f18f6defbe5e9e95fa42d88e07
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-11-30 09:34:50 +0330

    'CreateAdminPannel'

--- PayamShops/urls.py
+++ PayamShops/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path,include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('shop.urls'))
-]
+]+static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
 
 # python manage.py runserver
+
 # http://127.0.0.1:8000/hi/
+
+# python manage.py migrate -> create admin pannel
+
+# python manage.py createsuperuser -> create admin user
