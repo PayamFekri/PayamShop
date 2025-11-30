3137be2b184a1ffaee4b759ca4cd0e81ceccf8a8
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-11-30 22:50:35 +0330

    'added-authentication'

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render , redirect 
 from .models import Product
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
+
 def helloworld(request):
     all_product = Product.objects.all()
     return render(request, 'shop/index.html', {'ProductsINview' : all_product})
 
 def about(request):
     return render(request , 'shop/about.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request , username=username , password = password)
+        if user is not None : 
+            login(request , user)
+            messages.success(request , "Logged in successfully.")
+            return redirect("helloworld")
+        else:
+            messages.success(request , "There was a problem logging in.")
+            return redirect("login")
+    else :
+        return render(request , 'shop/login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request , ("You have successfully logged out."))
+    return redirect("helloworld")
