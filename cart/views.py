e901e69dafd8ba8d1592d918a8abc782cb092748
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-12-04 14:46:37 +0330

    'put-edit-in-cart'

--- cart/views.py
+++ cart/views.py
@@ -8,16 +8,18 @@ from django.http import JsonResponse
 def cart_summary(request):
     cart = Cart(request)
     cart_products = cart.get_prods()
-    return render(request , 'cart_summary.html' , {'cart_products':cart_products})
+    quantities = cart.get_quants()
+    return render(request , 'cart_summary.html' , {'cart_products':cart_products , 'quantities' :quantities})
 
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = request.POST.get('product_id')
+        product_qty = request.POST.get('product_qty')
         if not product_id:
             return JsonResponse({'error': 'No product id provided'}, status=400)
         product = get_object_or_404(Product, id=int(product_id))
-        cart.add(product=product)
+        cart.add(product=product , quantity = product_qty)
         cart_quantity  = cart.__len__()
         
         return JsonResponse({'product_name': product.name , 'qty': cart_quantity})
@@ -26,4 +28,14 @@ def cart_delete(request):
     pass
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        if not product_id:
+            return JsonResponse({'error': 'No product id provided'}, status=400)
+        product = get_object_or_404(Product, id=int(product_id))
+        cart.update(product=product , quantity = product_qty)        
+        return JsonResponse({'product_name': product.name , 'qty': product_qty})
+
