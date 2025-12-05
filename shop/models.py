f007d3fbee4fbf6cd00c7d724b434dc9752bd8ef
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-12-05 12:18:02 +0330

    'new-update-profile'

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MinValueValidator , MaxValueValidator
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
+class Profile(models.Model):
+    user = models.OneToOneField(User , on_delete= models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now= True)
+    phone = models.CharField(max_length=40,blank= True)
+    address1 = models.CharField(max_length=300,blank= True)
+    address2 = models.CharField(max_length=300,blank= True)
+    city = models.CharField(max_length=40,blank= True)
+    state = models.CharField(max_length=40,blank= True)
+    zipcode = models.CharField(max_length=40,blank= True)
+    country = models.CharField(max_length=40,default= 'Iran')
+    def __str__(self):
+        return self.user.username
+def create_profile(sender , instance , created , **kwargs):
+    if created:
+        user_profile = Profile(user = instance)
+        user_profile.save()
+post_save.connect(create_profile , sender=User)
 
 class Category(models.Model):
     name = models.CharField(max_length= 20)
