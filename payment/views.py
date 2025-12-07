c29a380d08a7eaf0975a1938b167088f24ab3af8
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-12-07 20:44:36 +0330

    'edit-fstring-in-views.payment'

--- payment/views.py
+++ payment/views.py
@@ -59,7 +59,7 @@ def process_order(request):
         user_shipping = request.session.get('user_shipping')
         full_name = user_shipping['shipping_full_name']
         email = user_shipping['shipping_email']
-        full_address = f'{user_shipping['shipping_address1']}\n{user_shipping['shipping_address2']}\n{user_shipping['shipping_city']}\n{user_shipping['shipping_state']}\n{user_shipping['shipping_zipcode']}\n{user_shipping['shipping_country']}\n'
+        full_address = f"{user_shipping['shipping_address1']}\n{user_shipping['shipping_address2']}\n{user_shipping['shipping_city']}\n{user_shipping['shipping_state']}\n{user_shipping['shipping_zipcode']}\n{user_shipping['shipping_country']}\n"
         
         if request.user.is_authenticated:
             user = request.user
